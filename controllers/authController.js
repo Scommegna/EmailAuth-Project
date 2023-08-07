3495a625ec3ba24d1ea98216c0afe38c9bda18bf
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-07 17:14:42 -0300

    feat: sending email with front-end

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const { attachCookiesToResponse, createTokenUser } = require("../utils");
 const crypto = require("crypto");
+const sendEmail = require("../utils/sendEmail");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -26,17 +27,33 @@ const register = async (req, res) => {
     verificationToken,
   });
 
+  await sendEmail();
+
   res.status(StatusCodes.CREATED).json({
     msg: "Success! Please, check your email to verify your account.",
-    verificationToken: user.verificationToken,
   });
 };
 
 // Email verification
 const verifyEmail = async (req, res) => {
   const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
 
-  res.status(StatusCodes.OK).json({ verificationToken, email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Verification failed.");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification failed.");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Email verified." });
 };
 
 const login = async (req, res) => {
---
+++ utils/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+// Sends email to user with nodemailer and ethereal
+const sendEmail = async () => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "mustafa.kshlerin72@ethereal.email",
+      pass: "USb9JDjMDt7wRNeD6C",
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+};
+
+module.exports = sendEmail;
